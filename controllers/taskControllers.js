df750e2c6f209dbaf7bfe26175b90116c9541e49
Afnaanshaalle110 <luulabdullahishaale@gmail.com>
2024-11-02 12:01:14 -0700

    taskControllers.js

--- controllers/taskControllers.js
+++ controllers/taskControllers.js
@@ -1,3 +1,7 @@
+const { readTasksFromFile } = require("../utils/fileHandaler")
+
 exports.getTasks = (req, res) => {
-    
+    const tasks = readTasksFromFile();
+    res.writeHead(200,{'content-type': 'application/json'})
+
 }
