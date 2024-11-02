4a027431ef34d69dde9be7923da5360499b3bb4a
Afnaanshaalle110 <luulabdullahishaale@gmail.com>
2024-11-02 10:34:18 -0700

    taskRoutes.js

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -1,3 +1,17 @@
 const taskRoutes = (req , res) =>{
-    if(req.method ==)
+    if(req.method = 'GET') {
+        getTasksASKS(req, res);
+    } else if(req.method == 'POST') {
+        createTaskTask(req, res)
+    } else if(req.method = 'PATCH') {
+        updateTask(req, res)
+    } else if(req.method = 'DELETE') {
+        deleteTask(req, res)
+    } else {
+        res.writeHead(404, 'data not found', {'content-type': 'application/json'})
+        res.end(json.stringfy({
+            message: "unknown method required."
+        }))
+    }
+
 }
