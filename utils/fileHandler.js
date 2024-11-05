a40df733997f9edec25b861b450fafa17bbb161a
Afnaanshaalle110 <luulabdullahishaale@gmail.com>
2024-11-04 20:12:49 -0800

    tasks.json

--- utils/fileHandler.js
+++ utils/fileHandler.js
@@ -4,7 +4,7 @@ const path = require('path')
 const filePath = './data/tasks.json';
 
 exports.writeTasksTofile = (tasks) => {
-    fs.writeFileSync(filePath, JSON.stringify(tasks))
+    fs.writeFileSync(filePath, JSON.stringify(tasks, null, 2))
 }
 exports.readTasksFromFile = () => {
     if(!fs.existsSync(filePath)) {
